50c25dcf4418957aa4cde0283d27f765bc1a0984
Alexis <fkahope@Mac.local>
2024-06-30 14:20:22 +0200

    postman parse error

--- src/main/java/com/restapi/crudproject/controller/UserController.java
+++ src/main/java/com/restapi/crudproject/controller/UserController.java
@@ -1,29 +1,36 @@
 package com.restapi.crudproject.controller;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.restapi.crudproject.model.User;
 import com.restapi.crudproject.repository.UserRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @RestController
-@RequestMapping("/users")
+//Gestion des requetes HTTP
 public class UserController {
+
     @Autowired
+    //Injection de dépendance dans une classe
     private UserRepository userRepository;
 
-    @GetMapping("/test")
-    public String testConnection() {
-        // Créez un nouvel utilisateur pour tester la connexion
-        User user = new User();
-        user.setName("Test User");
-        userRepository.save(user);
+   
+    
+    @PostMapping("/user")
+    //L'annotation @PostMapping est utilisée pour mapper une requête HTTP POST à une méthode spécifique dans votre contrôleur. En d'autres termes, elle indique que lorsque le serveur reçoit une requête POST à une certaine URL, il doit appeler la méthode annotée.
+    User newUser(@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
 
-        // Vérifiez si l'utilisateur a été enregistré
-        long count = userRepository.count();
-        return "Nombre d'utilisateurs dans la base de données : " + count;
+    @GetMapping("/users")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
     }
 }
